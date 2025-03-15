02d33ebc6f25b8b4d7039045f34881b908973a15
remleknl <91032385+remleknl@users.noreply.github.com>
2025-03-15 23:19:04 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,31 +1,43 @@
 import 'package:flutter/material.dart';
-import '../services/recipe_service.dart';
+import 'package:provider/provider.dart';
 import '../models/recipe.dart';
+import '../providers/recipe_provider.dart';
 
 class HomeScreen extends StatelessWidget {
-  final RecipeService recipeService = RecipeService();
-
   @override
   Widget build(BuildContext context) {
+    final recipeProvider = Provider.of<RecipeProvider>(context);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Food Recipes')),
-      body: FutureBuilder<List<Recipe>>(
-        future: recipeService.getRecipes(),
+      appBar: AppBar(
+        title: Text('Food Recipes'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              // Logout functionality
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: recipeProvider.fetchRecipes(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          } else if (!snapshot.hasData || recipeProvider.recipes.isEmpty) {
             return Center(child: Text('No recipes found.'));
           } else {
-            List<Recipe> recipes = snapshot.data!;
+            List<Recipe> recipes = recipeProvider.recipes;
             return ListView.builder(
               itemCount: recipes.length,
               itemBuilder: (context, index) {
                 return ListTile(
                   title: Text(recipes[index].title),
                   subtitle: Text(recipes[index].description),
+                  leading: Image.network(recipes[index].imageUrl),
                 );
               },
             );
